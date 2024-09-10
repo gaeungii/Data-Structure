b8f51397f0c47c5d107bacb680aa47dd165fcc71
gaeun.jang <cscom222@naver.com>
2024-09-10 16:08:02 +0900

    Create ArrayQueueTest.java

---
+++ Queue/ArrayQueue/ArrayQueueTest.java
@@ -0,0 +1,23 @@
+package chap5;
+
+public class ArrayQueueTest {
+
+	public static void main(String[] args) {
+		ArrayQueue queue = new ArrayQueue(1000);
+		
+		queue.push(1);
+		queue.push(2);
+		queue.push(3);
+		queue.push(4);
+		queue.printQueue();
+		
+		queue.pop();
+		queue.pop();
+		queue.push(6);
+		queue.printQueue();
+		
+		System.out.println(queue.peek());
+		
+	}
+
+}
